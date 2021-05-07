d5fea27aaabff4d9f0f45564e0df06bc7bb013a9
louzx <12345678>
2021-05-07 08:45:58 +0800

    初始化提交

--- src/main/java/com/louzx/demo/mapper/IDemoDB1Mapper.java
+++ src/main/java/com/louzx/demo/mapper/IDemoDB1Mapper.java
@@ -1,11 +1,12 @@
 package com.louzx.demo.mapper;
 
 import com.louzx.multidatasource.annotation.DataSource;
+import com.louzx.multidatasource.annotation.MultiDataSource;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 @DataSource("db1")
-public interface IDemoDB1Mapper {
+public interface IDemoDB1Mapper extends MultiDataSource {
 
     void save();
 }
